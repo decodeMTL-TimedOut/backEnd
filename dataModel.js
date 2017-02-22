2d9cc620c2247a8bcf29e0ff55eb9d66555f787f
LeBlanc-Bryan <mrbeardbaron@gmail.com>
2017-02-22 16:47:44 -0500

    merge

--- dataModel.js
+++ dataModel.js
@@ -1,9 +1,5 @@
 /main - list of games
-<<<<<<< HEAD
-  [{
-=======
-  {
->>>>>>> origin/master
+[{
     id: 12345,
     name: `World of Warcraft`,
     art: `http://some.img.com/121234123.jpg`,
@@ -44,11 +40,7 @@
     },
     {
       game3
-<<<<<<< HEAD
     }]
-=======
-    }
->>>>>>> origin/master
 
 /games/:id - list of parties
   {
@@ -95,7 +87,6 @@
     startTime: date(),
     endTime: date(),
     status: `open`,
-<<<<<<< HEAD
       registrations:
         {
           id: 123,
@@ -105,33 +96,6 @@
           username: pickleBoy123,
           email: pickleBoy@gmail.com
         }
-=======
-      game: {
-        id: 123,
-        name: `World of Warcraft`,
-        art: `http://some.img.com/121234123.jpg`,
-        aliases: [`wow`, `legion`],
-        platform: `pc`,
-        description: `the world's biggest time sink`,
-        popularity: 100,
-      },
-        registrations: [
-          {
-            id: 123,
-            userId: 123,
-            joined: date(),
-            left: date()
-            username: pickleBoy123,
-            email: pickleBoy@gmail.com
-          },
-          {
-            member2
-          },
-          {
-            member3
-          }
-        ]
->>>>>>> origin/master
   }
 
    /games/:id/parties/:partyId/edit [post] - edit existing party
@@ -140,59 +104,13 @@
       name: `Ganking noobs in Westfall!`,
       startTime: date(),
       endTime: date(),
-<<<<<<< HEAD
       status: `open|confirmed`
-=======
-      status: `open|confirmed`,
-        game: {
-          id: 123,
-          name: `World of Warcraft`,
-          art: `http://some.img.com/121234123.jpg`,
-          aliases: [`wow`, `legion`],
-          platform: `pc`,
-          description: `the world's biggest time sink`,
-          popularity: 100,
-        },
-          registrations: [
-            {
-              id: 123,
-              userId: 123,
-              joined: date(),
-              left: date()
-              username: pickleBoy123,
-              email: pickleBoy@gmail.com
-            },
-            {
-              member2
-            },
-            {
-              member3
-            }
-          ]
->>>>>>> origin/master
     }
 
    /games/:id/parties/:partyId/join [post] - join a party
     {
       id: 123,
-<<<<<<< HEAD
           registrations:
-=======
-      name: `Ganking noobs in Westfall!`,
-      startTime: date(),
-      endTime: date(),
-      status: `open|confirmed`,
-        game: {
-          id: 123,
-          name: `World of Warcraft`,
-          art: `http://some.img.com/121234123.jpg`,
-          aliases: [`wow`, `legion`],
-          platform: `pc`,
-          description: `the world's biggest time sink`,
-          popularity: 100,
-        },
-          registrations: [
->>>>>>> origin/master
             {
               id: 123,
               userId: 123,
@@ -200,9 +118,6 @@
               left: date()
               username: pickleBoy123,
               email: pickleBoy@gmail.com
-<<<<<<< HEAD
-            }
-=======
             },
             {
               member2
@@ -210,98 +125,29 @@
             {
               member3
             }
-          ]
->>>>>>> origin/master
     }
 
    /games/:id/parties/:partyId/leave [post] - leave party or kick player
     {
       id: 123,
-<<<<<<< HEAD
           registrations:
             {
               id: 123,
             }
-=======
-      name: `Ganking noobs in Westfall!`,
-      startTime: date(),
-      endTime: date(),
-      status: `open|confirmed`,
-        game: {
-          id: 123,
-          name: `World of Warcraft`,
-          art: `http://some.img.com/121234123.jpg`,
-          aliases: [`wow`, `legion`],
-          platform: `pc`,
-          description: `the world's biggest time sink`,
-          popularity: 100,
-        },
-          registrations: [
-            {
-              id: 123,
-              userId: 123,
-              joined: date(),
-              left: date()
-              username: pickleBoy123,
-              email: pickleBoy@gmail.com
-            },
-            {
-              member2
-            },
-            {
-              member3
-            }
-          ]
->>>>>>> origin/master
-    }
+}
 
    /games/:id/parties/:partyId/delete [post] - delete party
     {
-<<<<<<< HEAD
       id: 123
-=======
-      id: 123,
-      name: `Ganking noobs in Westfall!`,
-      startTime: date(),
-      endTime: date(),
-      status: `open|confirmed`,
-        game: {
-          id: 123,
-          name: `World of Warcraft`,
-          art: `http://some.img.com/121234123.jpg`,
-          aliases: [`wow`, `legion`],
-          platform: `pc`,
-          description: `the world's biggest time sink`,
-          popularity: 100,
-        },
-          registrations: [
-            {
-              id: 123,
-              userId: 123,
-              joined: date(),
-              left: date()
-              username: pickleBoy123,
-              email: pickleBoy@gmail.com
-            },
-            {
-              member2
-            },
-            {
-              member3
-            }
-          ]
->>>>>>> origin/master
     }
 
    /games/:id/parties/:partyId/confirm [post]
     {
       id: 123,
-<<<<<<< HEAD
     }
 
 /search?s=SEARCHGAME_QUERY
 [{
-=======
       name: `Ganking noobs in Westfall!`,
       startTime: date(),
       endTime: date(),
@@ -335,7 +181,6 @@
 
 /search?s=SEARCHGAME_QUERY
 {
->>>>>>> origin/master
   id: 12345,
   name: `World of Warcraft`,
   art: `http://some.img.com/121234123.jpg`,
@@ -376,7 +221,6 @@
   },
   {
     game3
-<<<<<<< HEAD
   }]
 
 
@@ -423,48 +267,4 @@
   {
     game3
   }]
-
-=======
   }
-
-/search?s=SEARCHPARTY_QUERY
-
-  {
-    id: 12345,
-    name: `World of Warcraft`,
-    art: `http://some.img.com/121234123.jpg`,
-    aliases: [`wow`, `legion`],
-    platform: `pc`,
-    description: `the world's biggest time sink`,
-    popularity: 100,
-       parties : [
-         {
-           id: 54321,
-           name: `ganking noobs in Westfall!`,
-           startTime: date(),
-           endTime: date(),
-           status: `open|confirmed`,
-             players: [
-               {
-                 id: 43513,
-                 username: pickleBoy123
-               },
-               {
-                 player2
-               },
-               {
-                 player3
-               }
-           ]
-         },
-         {
-           party2
-         },
-         {
-           party3
-         }
-       ]
-  }
->>>>>>> origin/master
-
- A list of search result from the system game DB and external api
